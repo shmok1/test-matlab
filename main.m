baa610f778135813185f2ace8c600ede059b5a8f
sunghoonmok <sunghoonmok@gmail.com>
2025-08-22 11:39:44 +0900

    main함수 수정

--- main.m
+++ main.m
@@ -5,6 +5,6 @@ addpath(genpath('./lib'));
 
 y = fun( a , b );
 
-fprintf( 'a = %3d \n' , a );
-fprintf( 'b = %3d \n' , b );
+% fprintf( 'a = %3d \n' , a );
+% fprintf( 'b = %3d \n' , b );
 fprintf( "y = %3d \n" , y );
